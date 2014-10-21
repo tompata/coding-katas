2366dd5115e00aa0ac9fe78fb4b99fb1002fb497
Tamas Tompa <tamastompa@TT-MacBook.local>
2014-10-21 13:13:49 +0200

    phase 1 - with 1 failing test

--- string-calculator/src/calculator.js
+++ string-calculator/src/calculator.js
@@ -1,6 +1,22 @@
 
-function calculator() {
+function calculator(str) {
+	if (str == "") {
+		return 0;
+	} 
 
+	var sum = 0;
+	var del = ',';
+
+	if (str[0] == "\\" && str[1] == "\\" ) {
+		del = str[2];
+	}
+
+	var nums = str.split(/,|\n/);
+
+	for (var i = 0; i < nums.length; i++ ) {
+		sum += parseInt(nums[i]);
+	}
+
+	return sum;
 }
-
 module.exports.calculator = calculator
--- string-calculator/test/test.js
+++ string-calculator/test/test.js
@@ -3,5 +3,40 @@ var assert = require("assert")
 var calculator = require('../src/calculator.js').calculator;
 
 suite('calculator', function() {
-
+	test('empty input', function() {
+		assert.equal( 0, calculator("") );
+	});
+	test('1 should return back 1', function() {
+		assert.equal( 1, calculator("1") );
+	});
+	test('8 should return back 8', function() {
+		assert.equal( 8, calculator("8") );
+	});
+	test('8,1 should return back 9', function() {
+		assert.equal( 9, calculator("8,1") );
+	});
+	test('18 should return back 18', function() {
+		assert.equal( 18, calculator("18") );
+	});
+	test('18, 1 should return back 19', function() {
+		assert.equal( 19, calculator("18, 1") );
+	});
+	test('-5 should return back -5', function() {
+		assert.equal( -5, calculator("-5") );
+	});
+	test('-5, -8 should return back -13', function() {
+		assert.equal( -13, calculator("-5, -8") );
+	});
+	test('1,2,3 should return back 6', function() {
+		assert.equal( 6, calculator("1,2,3") );
+	});
+	test('1,2,3,4,5 should return back 15', function(){
+		assert.equal( 15, calculator("1,2,3,4,5") );
+	});
+	test('1,2,3\n4,5 should return back 15', function(){
+		assert.equal( 15, calculator("1,2,3\n4,5") );
+	});
+	test('//;\n1;2 should return back 3', function(){
+	 	assert.equal( 3, calculator("//;\n1;2") );
+	});
 });
