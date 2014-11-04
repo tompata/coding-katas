478b7841b5e49138c4bef3dfc3d1949d5d3ed5ed
Tamas Tompa <tamastompa@tt-macbook.bp.prezi.private>
2014-11-04 11:16:01 +0100

    inifinite loop fixed

--- merge-arrays/src/mergearrays.js
+++ merge-arrays/src/mergearrays.js
@@ -13,8 +13,12 @@ function mergearrays(arr1, arr2) {
 	var arr3 = [];
 	var length = arr1.length;
 	var i = 0, j = 0;
-
+	// var c = 0;
 	while (i < length || j < length) {
+		console.log(arr3 + " | " + i + " | " + j);
+		if (j==length) {
+			return arr3.concat(arr1.slice(i));
+		}
 		if (arr1[i] < arr2[j]) {
 			arr3.push(arr1[i]);
 			i++;
@@ -22,6 +26,10 @@ function mergearrays(arr1, arr2) {
 			arr3.push(arr2[j]);
 			j++;
 		}
+		// if (c == 20) {
+		// 	break;
+		// }
+		// c++;
 	}
 	return arr3;
 }
--- merge-arrays/test/test.js
+++ merge-arrays/test/test.js
@@ -7,6 +7,9 @@ suite('mergearrays', function() {
 	test('[1,3,5] and [2,4,6] should return [1,2,3,4,5,6]', function(){
 		assert.deepEqual([1,2,3,4,5,6], mergearrays([1,3,5], [2,4,6]));
 	});
+	test('[1,3,5] and [1,3,5] should return [1,1,3,3,5,5]', function(){
+		assert.deepEqual([1,1,3,3,5,5], mergearrays([1,3,5], [1,3,5]));
+	});
 });
 
 // suite('concatenate', function() {
