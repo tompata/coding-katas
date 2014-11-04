5632a7908d7eb5fb95880913e6ba07a7f0381ef1
Tamas Tompa <tamastompa@tt-macbook.bp.prezi.private>
2014-11-04 11:18:48 +0100

    finalised merge array; cleanup

--- merge-arrays/src/mergearrays.js
+++ merge-arrays/src/mergearrays.js
@@ -1,21 +1,8 @@
-
-// function concatenate(arr1, arr2) {
-// 	return arr1.concat(arr2); 
-// }
-
-// function sorted(arr) {
-	 
-// }
-
 function mergearrays(arr1, arr2) {
-	// var arr3 = concatenate(arr1, arr2);
-	// return sorted(arr3);
 	var arr3 = [];
 	var length = arr1.length;
 	var i = 0, j = 0;
-	// var c = 0;
 	while (i < length || j < length) {
-		console.log(arr3 + " | " + i + " | " + j);
 		if (j==length) {
 			return arr3.concat(arr1.slice(i));
 		}
@@ -26,14 +13,8 @@ function mergearrays(arr1, arr2) {
 			arr3.push(arr2[j]);
 			j++;
 		}
-		// if (c == 20) {
-		// 	break;
-		// }
-		// c++;
 	}
 	return arr3;
 }
 
-
 module.exports.mergearrays = mergearrays;
-// module.exports.concatenate = concatenate;
--- merge-arrays/test/test.js
+++ merge-arrays/test/test.js
@@ -10,6 +10,12 @@ suite('mergearrays', function() {
 	test('[1,3,5] and [1,3,5] should return [1,1,3,3,5,5]', function(){
 		assert.deepEqual([1,1,3,3,5,5], mergearrays([1,3,5], [1,3,5]));
 	});
+	test('[] and [] should return []', function(){
+		assert.deepEqual([], mergearrays([], []));
+	});
+	test('[2,4,6],[1,3,5] should return [1,2,3,4,5,6]', function(){
+		assert.deepEqual([1,2,3,4,5,6], mergearrays( [2,4,6],[1,3,5]));
+	});
 });
 
 // suite('concatenate', function() {
