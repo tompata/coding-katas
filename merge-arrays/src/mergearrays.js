622a64154df5a59fa233d4856b394b79c594e269
Tamas Tompa <tamastompa@tt-macbook.bp.prezi.private>
2014-11-04 11:00:29 +0100

    mergearrays implemented

--- merge-arrays/src/mergearrays.js
+++ merge-arrays/src/mergearrays.js
@@ -1,12 +1,31 @@
 
+// function concatenate(arr1, arr2) {
+// 	return arr1.concat(arr2); 
+// }
+
+// function sorted(arr) {
+	 
+// }
+
 function mergearrays(arr1, arr2) {
-	var arr3 = concatenate(arr1, arr2);
-	return sorted(arr3);
+	// var arr3 = concatenate(arr1, arr2);
+	// return sorted(arr3);
+	var arr3 = [];
+	var length = arr1.length;
+	var i = 0, j = 0;
+
+	while (i < length || j < length) {
+		if (arr1[i] < arr2[j]) {
+			arr3.push(arr1[i]);
+			i++;
+		} else {
+			arr3.push(arr2[j]);
+			j++;
+		}
+	}
+	return arr3;
 }
 
-function concatenate(arr1, arr2) {
-	return arr1.concat(arr2); 
-}
 
 module.exports.mergearrays = mergearrays;
-module.exports.concatenate = concatenate;
+// module.exports.concatenate = concatenate;
--- merge-arrays/test/test.js
+++ merge-arrays/test/test.js
@@ -1,7 +1,7 @@
 
 var assert = require("assert")
 var mergearrays = require('../src/mergearrays.js').mergearrays;
-var concatenate = require('../src/mergearrays.js').concatenate;
+// var concatenate = require('../src/mergearrays.js').concatenate;
 
 suite('mergearrays', function() {
 	test('[1,3,5] and [2,4,6] should return [1,2,3,4,5,6]', function(){
@@ -9,8 +9,8 @@ suite('mergearrays', function() {
 	});
 });
 
-suite('concatenate', function() {
-	test('[1,3,5] and [2,4,6] should return [1,3,5,2,4,6]', function(){
-		assert.deepEqual([1,3,5,2,4,6], concatenate([1,3,5], [2,4,6]));
-	});
-});
+// suite('concatenate', function() {
+// 	test('[1,3,5] and [2,4,6] should return [1,3,5,2,4,6]', function(){
+// 		assert.deepEqual([1,3,5,2,4,6], concatenate([1,3,5], [2,4,6]));
+// 	});
+// });
