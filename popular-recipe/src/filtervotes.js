572fcf0354e3784493659d74603c5f322d8ca8be
Ferenc Murcsik <ferencmurcsik@ferencs-mbp.bp.prezi.private>
2014-11-25 11:16:28 +0100

    first test pass

--- popular-recipe/src/filtervotes.js
+++ popular-recipe/src/filtervotes.js
@@ -1,19 +1,13 @@
 var fs = require('fs');
 
 
-function parseInput(filename) {
-	var bufferString, bufferStringSplit;
-	function readFile() {
-		fs.readFile(filename, function (err, data) {
-		bufferString = data.toString(); 
-		bufferStringSplit = bufferString.split('\n'); 
-		// callback();
-		});
-	}
-	readFile();
-	cmd.log(bufferStringSplit);
+function parseInput(filename){
+	var returnLines=[];
+	fs = require('fs')
+	returnLines = fs.readFileSync(filename).toString();
+	returnMap = returnLines.split("\n").filter(function (element){return element.length > 0;});
+	return returnMap;
 }
-
 
 function filtervotes(filename) {
 	var inputLines = parseInput(filename)
@@ -23,7 +17,6 @@ function filtervotes(filename) {
 
 module.exports.filtervotes = filtervotes;
 module.exports.parseInput = parseInput;
-// function printLine() {
-//   console.log(bufferStringSplit);
+
 // }
  
--- popular-recipe/test/test.js
+++ popular-recipe/test/test.js
@@ -7,7 +7,7 @@ suite('filter recipe votes', function() {
 	// 	assert.deepEqual([0],filtervotes('../4.txt'));
 	// });
 	test('4.txt should result [0]', function() {
-		assert.deepEqual(["0"],parseInput("../4.txt"));
+		assert.deepEqual(["0"],parseInput("4.txt"));
 	});
 
 });
