fbe382470b057acf97e18f90f447629d9f9444fc
Ferenc Murcsik <ferencmurcsik@ferencs-mbp.bp.prezi.private>
2014-11-25 10:55:33 +0100

    first steps

--- popular-recipe/src/filtervotes.js
+++ popular-recipe/src/filtervotes.js
@@ -1,6 +1,29 @@
+var fs = require('fs');
 
-function filtervotes() {
+
+function parseInput(filename) {
+	var bufferString, bufferStringSplit;
+	function readFile() {
+		fs.readFile(filename, function (err, data) {
+		bufferString = data.toString(); 
+		bufferStringSplit = bufferString.split('\n'); 
+		// callback();
+		});
+	}
+	readFile();
+	cmd.log(bufferStringSplit);
+}
+
+
+function filtervotes(filename) {
+	var inputLines = parseInput(filename)
+	
 
 }
 
 module.exports.filtervotes = filtervotes;
+module.exports.parseInput = parseInput;
+// function printLine() {
+//   console.log(bufferStringSplit);
+// }
+ 
--- popular-recipe/test/test.js
+++ popular-recipe/test/test.js
@@ -1,7 +1,13 @@
-
-var assert = require("assert")
+var assert = require("chai").assert;
 var filtervotes = require('../src/filtervotes.js').filtervotes;
+var parseInput = require('../src/filtervotes.js').parseInput;
 
 suite('filter recipe votes', function() {
+	// test('', function() {
+	// 	assert.deepEqual([0],filtervotes('../4.txt'));
+	// });
+	test('4.txt should result [0]', function() {
+		assert.deepEqual(["0"],parseInput("../4.txt"));
+	});
 
 });
