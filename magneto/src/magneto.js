c6224d0b68f03e8c2cfed3875abfde1589e8a5e1
Ferenc Murcsik <ferencmurcsik@ferencs-mbp.bp.prezi.private>
2014-11-27 10:45:36 +0100

    2 passing test for good and bad pos

---
+++ magneto/src/magneto.js
@@ -0,0 +1,9 @@
+
+var magnetoEffect = { 
+	method1: function( param ) {
+	},
+	method2: function( param ) {
+	}
+};
+
+module.exports.magnetoEffect = magnetoEffect;
---
+++ magneto/test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var assert = require('assert');
+
+var magnetoEffect = require('../src/magneto.js').magnetoEffect;
+
+suite('magnetoEffect', function() {
+
+  test( 'test', function() {
+    
+  });
+
+});
