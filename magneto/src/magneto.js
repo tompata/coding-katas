1401923ec0df5a167dfd502850437e7fef8e288a
Ferenc Murcsik <ferencmurcsik@ferencs-mbp.bp.prezi.private>
2014-12-03 12:35:35 +0100

    magneto tests work

--- magneto/src/magneto.js
+++ magneto/src/magneto.js
@@ -1,9 +1,31 @@
+function xCheck(xMouse,xMagneto,radius){
+	if (Math.abs(xMouse - xMagneto) <= radius)
+		return xMagneto;
+	else
+		return xMouse;
+}
 
-var magnetoEffect = { 
-	method1: function( param ) {
-	},
-	method2: function( param ) {
+function yCheck(yMouse,yMagneto,radius){
+	if (Math.abs(yMouse - yMagneto) <= radius)
+		return yMagneto;
+	else
+		return yMouse;
+}
+
+
+
+function magnetoEffect (mousePos,magnetoPos,radius){ 
+	// method1: function( param ) {
+	// },
+	// method2: function( param ) {
+	// }
+	xDrawPoint = [];
+	yDrawPoint = [];
+	for ( i = 0; i < magnetoPos.length; i++) {
+		mousePos[0] = xCheck(mousePos[0],magnetoPos[i][0],radius);
+		mousePos[1] = yCheck(mousePos[1],magnetoPos[i][1],radius);
 	}
+	return mousePos;
 };
 
 module.exports.magnetoEffect = magnetoEffect;
--- magneto/test/test.js
+++ magneto/test/test.js
@@ -6,8 +6,24 @@ var magnetoEffect = require('../src/magneto.js').magnetoEffect;
 
 suite('magnetoEffect', function() {
 
-  test( 'test', function() {
-    
+  test( '[49,50] should return draw points at [50,50]', function() {
+  assert.deepEqual([50,50],magnetoEffect([49,50],[[50,50]],5));  
   });
+
+  test( '[0,0] should return draw points at [0,0]', function() {
+  assert.deepEqual([0,0],magnetoEffect([0,0],[[50,50]],5));  
+  });
+
+  test( '[101,48] should return draw points at [100,50]', function() {
+  assert.deepEqual([100,50],magnetoEffect([101,48],[[50,50],[100,50]],5));  
+  });
+
+  test( '[51,52] should return draw points at [51,51]', function() {
+  	assert.deepEqual([51,51],magnetoEffect([51,52],[[50,50],[51,51]],5));  
+ 	 });
+    
+   // test( '[51,52] should return draw points at [51,51]', function() {
+ 	 // assert.deepEqual([51,51],magnetoEffect([51,52],[[51,51],[50,50]],5));  
+  	// });   
 
 });
